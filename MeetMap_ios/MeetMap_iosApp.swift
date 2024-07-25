ad0530f2c1f182a620439a405fa223470f6dd148
Ilya <ilaprokofev14@gmail.com>
2024-07-25 16:15:58 +0300

    Initial Commit

---
+++ MeetMap_ios/MeetMap_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetMap_iosApp.swift
+//  MeetMap_ios
+//
+//  Created by Ilya Prokofev on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MeetMap_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
